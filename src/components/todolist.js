dec0e02191755ee3a4da3d901e3521f23770ffcb
grz3chotnik <krzysztofkubinski2004@gmail.com>
2026-02-09 10:53:32 -0300

    initial commit

---
+++ src/components/todolist.js
@@ -0,0 +1,29 @@
+import {useState} from "react";
+
+const TodoList = () => {
+
+    const [listItems, setListItems] = useState([])
+    const [inputValue, setInputValue] = useState()
+    const inputHandler = (event) => {
+        setInputValue(event.target.value)
+    }
+
+    return (
+        <>
+        <div>
+
+            <ul>
+                {listItems.map((item) => <li>{item}</li>)}
+            </ul>
+        </div>
+
+
+            <input value={inputValue} onChange={inputHandler} />
+        <div>
+            <button onClick={() => {setListItems(inputValue)}} >add</button>
+        </div>
+        </>
+    )
+}
+
+export default TodoList
